2587c8917746a0ab29d1a71a406e4c47b07a41d3
PeacefulBoyOfCourse <golubeffegor@yandex.ru>
2022-07-12 22:36:50 +0300

    module1 completed

---
+++ module 1/ex1-6-11-unique-selectors.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    first_name_field = browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']")
+    first_name_field.send_keys("Имя")
+    time.sleep(2)
+
+    second_name_field = browser.find_element(By.XPATH, "//input[@placeholder='Input your last name']")
+    second_name_field.send_keys("Фамилия")
+    time.sleep(2)
+
+    email_field = browser.find_element(By.XPATH, "//input[@placeholder='Input your email']")
+    email_field.send_keys("Почтовый адрес")
+    time.sleep(2)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
